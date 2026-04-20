5e6e508438d60908603c2b54c3e14537b2a1e203
Artem12378 <susidko.2012@mail.ru>
2026-04-20 22:44:05 +0300

    function++/ hof / callback/

--- main.js
+++ main.js
@@ -1,31 +1,67 @@
-/* Задание 1: Работа с циклом for
-Создайте массив numbers с числами от 1 до 10. Используя цикл for, выведите в консоль каждое число, умноженное на 2. */
+/* Задание 1: Функции высшего порядка
+Создайте функцию высшего порядка filterArray, которая:
 
-let numbers = [1,2,3,4,5,6,7,8,9,10]
+Принимает массив и функцию-предикат (функцию, возвращающую true или false)
+Возвращает новый массив, содержащий только те элементы, для которых предикат вернул true
+Протестируйте функцию с массивом чисел, отфильтровав: const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; const evenNumbers = filterArray(numbers, num => num % 2 === 0); console.log(evenNumbers); const bigNumbers = filterArray(numbers, num => num > 5); console.log(bigNumbers); */
 
-for(i=0; i <= numbers.length; i++){
-    console.log(numbers[i]*2)
+function filterArray(array, funcPred){
+    const result = [];
+    for(let i =0 ; i < array.length; i++){
+        if(funcPred(array[i])) {
+            result.push(array[i])
+        }
+    }
+    return result
 }
 
-/* Задание 3: Цикл while
-Напишите программу, которая генерирует случайные числа от 1 до 10 с помощью Math.random(), пока не выпадет число 7. Используйте цикл while и выводите каждое сгенерированное число в консоль. */
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
+const evenNumbers = filterArray(numbers, num => num % 2 === 0); 
+console.log(evenNumbers); 
+const bigNumbers = filterArray(numbers, num => num > 5); 
+console.log(bigNumbers)
 
-let randomNumber;
-while (randomNumber !== 7) {
-    randomNumber = Math.floor(Math.random() * 10) + 1; 
-    console.log(randomNumber);
+
+/* Задание 2: Параметры по умолчанию
+Создайте функцию createUser(name, age, city)
+Функция должна возвращать объект с этими свойствами
+Протестируйте функцию, вызвав её: // Все параметры по умолчанию const user1 = createUser(); console.log(user1); // {name: "Гость", age: 18, city: "Неизвестно"} // Только имя const user2 = createUser("Анна"); console.log(user2); // {name: "Анна", age: 18, city: "Неизвестно"} // Имя и возраст const user3 = createUser("Петр", 25); console.log(user3); // {name: "Петр", age: 25, city: "Неизвестно"} // Все параметры const user4 = createUser("Мария", 30, "Москва"); console.log(user4); // {name: "Мария", age: 30, city: "Москва"} */
+
+function createUser(name ='Гость' , age = 18, city = 'Неизвестно'){
+    return {name,age,city}
 }
-console.log("Выпало 7! Цикл завершён.");
 
-/* Задание 2: Перебор массива с помощью for
-Создайте массив colors с пятью названиями цветов. Используя цикл for и индексы элементов, выведите в консоль сообщения вида: "Цвет номер [номер]: [название цвета]". */
+const user1 = createUser(); 
+console.log(user1); 
+// Все параметры по умолчанию const user1 = createUser(); console.log(user1); //
+// {name: "Гость", age: 18, city: "Неизвестно"} 
+// // Только имя const user2 = createUser("Анна"); console.log(user2); 
+// // {name: "Анна", age: 18, city: "Неизвестно"} 
+// // Имя и возраст const user3 = createUser("Петр", 25); console.log(user3); 
+// // {name: "Петр", age: 25, city: "Неизвестно"} 
+// // Все параметры const user4 = createUser("Мария", 30, "Москва"); console.log(user4); 
+// // {name: "Мария", age: 30, city: "Москва"}
 
-const colors = ['green', 'red', 'blue', 'yellow', 'white'];
+/* Задание 3: Замыкания
+Создайте функцию createPasswordChecker, которая возвращает другую функцию с заранее заданным паролем.
+Пример использования:
+const checkPassword = createPasswordChecker("secret123");
 
-for(let i=0; i<=colors.length; i++){
-   
-    if(colors[i] === undefined) break;
-    console.log(`Цвет номер ${i+1}: ${colors[i]   }`)
-    
-    
+console.log(checkPassword("password")); // false
+
+console.log(checkPassword("secret123")); // true */
+
+function createPasswordChecker(num){
+    debugger
+    return function Pass (){
+        debugger
+               return  num === 'secret123' ? true : false
+            }
+    return Pass
 }
+
+const checkPassword = createPasswordChecker();
+
+console.log(checkPassword("password")); // false
+
+console.log(checkPassword("secret123")); // true */
