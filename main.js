092d8b6b91e32cc4341622166e1d2b5f0391dc6a
Artem12378 <susidko.2012@mail.ru>
2026-04-21 20:29:16 +0300

    Spred operator/Object destructuring

--- main.js
+++ main.js
@@ -1,67 +1,66 @@
-/* Задание 1: Функции высшего порядка
-Создайте функцию высшего порядка filterArray, которая:
+/* Задание 1: Деструктуризация массивов
+Создайте массив weekDays со значениями ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+Используя деструктуризацию, извлеките: 
+Выходные дни в массив weekend 
+Последний рабочий день недели в переменную lastWorkDay 
+Первый рабочий день недели в переменную firstWorkDay
+Выведите все полученные переменные в консоль */
 
-Принимает массив и функцию-предикат (функцию, возвращающую true или false)
-Возвращает новый массив, содержащий только те элементы, для которых предикат вернул true
-Протестируйте функцию с массивом чисел, отфильтровав: const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; const evenNumbers = filterArray(numbers, num => num % 2 === 0); console.log(evenNumbers); const bigNumbers = filterArray(numbers, num => num > 5); console.log(bigNumbers); */
+const weekDays = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 
-function filterArray(array, funcPred){
-    const result = [];
-    for(let i =0 ; i < array.length; i++){
-        if(funcPred(array[i])) {
-            result.push(array[i])
-        }
-    }
-    return result
+const [firstWorkDay, , , ,lastWorkDay ,...weekend] = weekDays
+console.log(firstWorkDay)
+console.log(lastWorkDay)
+console.log(weekend)
+
+
+/* Задание 2: Деструктуризация объектов
+Создайте объект person со следующими свойствами:
+
+
+Используя деструктуризацию, извлеките:
+
+Имя и фамилию в переменные name и surname
+Возраст в переменную personAge
+Массив навыков в переменную abilities
+Университет в переменную university
+ */
+
+const person = {
+    firstName:'Иван',
+    lastName: 'Петров',
+    age: 25,
+    country: 'Россия',
+    city: 'Москва',
+    skills:['JavaScript', 'HTML', 'CSS'],
+    education: {
+        degree:'Магистр',
+        university:'МГУ'
+    } 
+};
+
+const {firstName:name, lastName:surname, age:personAge, skills:abilities, education:{university} } = person
+console.log(name)
+console.log(personAge)
+console.log(abilities)
+console.log(university)
+
+
+/* Задание 3: Rest-оператор
+Создайте функцию calculateAverage, которая принимает неограниченное количество аргументов-чисел и возвращает их среднее арифметическое
+Используйте rest-оператор для сбора всех аргументов в массив
+Протестируйте функцию для разных наборов чисел:
+console.log(calculateAverage(5, 10, 15)); // 10
+
+console.log(calculateAverage(2, 4, 6, 8, 10)); // 6
+
+console.log(calculateAverage(1)); // 1
+
+console.log(calculateAverage()); // 0 или NaN или другое значение по умолчанию */
+
+function calculateAverage(...sum){
+    const newSum = [...sum].reduce((sumPlus,n) => sumPlus + n,0);
+    return newSum
 }
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
-const evenNumbers = filterArray(numbers, num => num % 2 === 0); 
-console.log(evenNumbers); 
-const bigNumbers = filterArray(numbers, num => num > 5); 
-console.log(bigNumbers)
-
-
-/* Задание 2: Параметры по умолчанию
-Создайте функцию createUser(name, age, city)
-Функция должна возвращать объект с этими свойствами
-Протестируйте функцию, вызвав её: // Все параметры по умолчанию const user1 = createUser(); console.log(user1); // {name: "Гость", age: 18, city: "Неизвестно"} // Только имя const user2 = createUser("Анна"); console.log(user2); // {name: "Анна", age: 18, city: "Неизвестно"} // Имя и возраст const user3 = createUser("Петр", 25); console.log(user3); // {name: "Петр", age: 25, city: "Неизвестно"} // Все параметры const user4 = createUser("Мария", 30, "Москва"); console.log(user4); // {name: "Мария", age: 30, city: "Москва"} */
-
-function createUser(name ='Гость' , age = 18, city = 'Неизвестно'){
-    return {name,age,city}
-}
-
-const user1 = createUser(); 
-console.log(user1); 
-// Все параметры по умолчанию const user1 = createUser(); console.log(user1); //
-// {name: "Гость", age: 18, city: "Неизвестно"} 
-// // Только имя const user2 = createUser("Анна"); console.log(user2); 
-// // {name: "Анна", age: 18, city: "Неизвестно"} 
-// // Имя и возраст const user3 = createUser("Петр", 25); console.log(user3); 
-// // {name: "Петр", age: 25, city: "Неизвестно"} 
-// // Все параметры const user4 = createUser("Мария", 30, "Москва"); console.log(user4); 
-// // {name: "Мария", age: 30, city: "Москва"}
-
-/* Задание 3: Замыкания
-Создайте функцию createPasswordChecker, которая возвращает другую функцию с заранее заданным паролем.
-Пример использования:
-const checkPassword = createPasswordChecker("secret123");
-
-console.log(checkPassword("password")); // false
-
-console.log(checkPassword("secret123")); // true */
-
-function createPasswordChecker(num){
-    debugger
-    return function Pass (){
-        debugger
-               return  num === 'secret123' ? true : false
-            }
-    return Pass
-}
-
-const checkPassword = createPasswordChecker();
-
-console.log(checkPassword("password")); // false
-
-console.log(checkPassword("secret123")); // true */
+console.log(calculateAverage(1,2,3,4,5,6,7,8,9,10))
